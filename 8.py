30188f8198fdc992d36709dbab2fbf1d0267f2d7
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-10-24 16:44:29 +0530

    Added new files

---
+++ 8.py
@@ -0,0 +1,14 @@
+# Q.8 Print:
+# 11   12    13   14   15   16
+# 21   22    23   24   25   26
+# 31   32    33   34   35   36
+# 41   42    43   44   45   46
+
+i = 1
+while i <= 4:
+    count = 11
+    while count <= 16:
+        print(count, end=" ")
+        count = count + 1
+    print()
+    i= i + 1
---
+++ Assignment-2/3.py
@@ -0,0 +1,8 @@
+# 3. Any year is input through the keyboard. Write a program to determine whether the year is a 
+# leap year or not.
+
+# year = int(input("Enter any year: "))
+# if (year % 400 == 0 & year % 4 == 0):
+#     print("It is a Leap year")
+# elif (year % 100 == 0):
+#     print("Not a leap year");
