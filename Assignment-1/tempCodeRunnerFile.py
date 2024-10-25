1859e6021b9df46ba2bede307b6117866160d712
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-10-25 20:46:19 +0530

    Added new files

---
+++ Assignment-1/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length of a rectangle: "));
+# breadth = float(input("Enter the breadth of a reactangle: "));
+
+# area = length * breadth;
+# perimeter = 2 * (length + breadth);
+
+# print(f"Area of Rectangle of {length} length and {breadth} breadth: {area}");
+# print(f"Perimeter of a Rectangle of {length} length and {breadth} breadth: {perimeter}");
