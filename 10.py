ab7fb8faa9328ad7ad1e19a0f955be6b499c44be
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-11-10 13:24:32 +0530

    Updated files

--- 10.py
+++ 10.py
@@ -1,11 +1,22 @@
 # If a five-digit number is input through the keyboard, write a program to calculate the sum of 
 # its digits.(Hint: Use the modulus operator ‘%’)
 
-num = int(input("Enter five digit number: "))
-num_to_str = str(num)       # converted to string
-listing = list(num_to_str)  # converted to list e.g ["1", "2", "3", "4", "5"]
-total_sum = 0
+# num = int(input("Enter five digit number: "))
+# num_to_str = str(num)       # converted to string
+# listing = list(num_to_str)  # converted to list e.g ["1", "2", "3", "4", "5"]
+# total_sum = 0
 
-for value in listing:
-    total_sum += int(value)
-print(total_sum)
+# for value in listing:
+#     total_sum += int(value)
+# print(total_sum)
+
+num = int(input("Enter a five-digit number: "))
+
+sum_of_digits = 0
+
+while num > 0:
+    digit = num % 10
+    sum_of_digits += digit
+    num //= 10
+
+print("Sum of digits:", sum_of_digits)
--- Assignment-1/practice.py
+++ Assignment-1/practice.py
@@ -1,18 +1,5 @@
 # If a five-digit number is input through the keyboard, write a program to reverse the number.
 
-# num = int(input("Enter five digit number: "))   #23456
-
-# if 10000 <= num <= 99999:
-#     rev_num = 0
-#     while num > 0:
-#         last_digit = num % 10
-#         rev_num = (rev_num * 10) + last_digit
-#         remove_digit = num // 10
-#     print(f"Reversed number: {rev_num}")
-# else:
-#     print("Enter a valid five digit number!")
-
-
 num = int(input("Enter a five-digit number: "))
 
 if 10000 <= num <= 99999:
