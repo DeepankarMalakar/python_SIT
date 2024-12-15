62929b0018c474fa9718ed1b7a2009f917ff80fb
DeepankarMalakar <deepankarmalakar007@gmail.com>
2024-12-15 18:49:23 +0530

    Updated files

--- practice.py
+++ practice.py
@@ -1,13 +1,4 @@
-x, y = map(int, input("Enter the coordinates of the point(x, y): ").split())
 
-if x == 0 and y == 0:
-    print("The point is at the origin")
-elif x == 0:
-    print("The point is at the x-axis")
-elif y == 0:
-    print("The point is at y-axis")
-else:
-    print("The point does not lie on either axis or the origin")
 # def factorial(n):
 #     fact = 1
 #     if n <= 0:
