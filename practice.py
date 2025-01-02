f6cc4a3f6f27dd0cf47caca9be5c6b794227b3e8
DeepankarMalakar <deepankarmalakar007@gmail.com>
2025-01-02 21:28:05 +0530

    Added

--- practice.py
+++ practice.py
@@ -92,3 +92,10 @@
 
 # obj = C()
 # obj.display()  # Output depends on Method Resolution Order (MRO) . priotirize the left most class
+
+#fibo
+n = int(input("Enter the number of terms: "))
+a, b = 0, 1
+for i in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
